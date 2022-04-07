836dd0e1d35c0c9403139f138699bed68d577224
SocheataHour <meimeihou88@gmail.com>
2022-04-06 19:02:30 -0700

    Add folder for restaurant info and add the storyboard and the view controller to the folder

---
+++ FoodStacks/FoodStacks/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodStacks
+//
+//  Created by Rafael Eduardo Lintao on 3/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        //test
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
